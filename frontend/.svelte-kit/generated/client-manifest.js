c1eafa1a099f547a3e1a91bb7393f60988b439f9
stefano <stefano.zini@dgalab.it>
2022-12-22 10:01:55 +0100

    clenaup

--- frontend/.svelte-kit/generated/client-manifest.js
+++ frontend/.svelte-kit/generated/client-manifest.js
@@ -2,12 +2,18 @@ export { matchers } from './client-matchers.js';
 
 export const nodes = [() => import('./nodes/0'),
 	() => import('./nodes/1'),
-	() => import('./nodes/2')];
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5')];
 
 export const server_loads = [];
 
 export const dictionary = {
-	"/": [2]
+	"/": [2],
+	"/about": [3],
+	"/sverdle": [~4],
+	"/sverdle/how-to-play": [5]
 };
 
 export const hooks = {
---
+++ frontend_/.svelte-kit/ambient.d.ts
@@ -0,0 +1,177 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const USE_POLLING: string;
+	export const npm_config_user_agent: string;
+	export const NODE_VERSION: string;
+	export const HOSTNAME: string;
+	export const YARN_VERSION: string;
+	export const npm_package_devDependencies_vite: string;
+	export const npm_node_execpath: string;
+	export const SHLVL: string;
+	export const HOME: string;
+	export const npm_package_devDependencies__typescript_eslint_parser: string;
+	export const npm_package_devDependencies_eslint_config_prettier: string;
+	export const npm_package_devDependencies_svelte_preprocess: string;
+	export const npm_package_devDependencies_svelte_check: string;
+	export const npm_package_scripts_check: string;
+	export const npm_config_engine_strict: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const npm_package_scripts_dev: string;
+	export const npm_package_devDependencies__playwright_test: string;
+	export const npm_package_devDependencies_prettier: string;
+	export const npm_package_type: string;
+	export const npm_package_private: string;
+	export const npm_package_scripts_check_watch: string;
+	export const npm_package_devDependencies__sveltejs_adapter_node: string;
+	export const npm_package_scripts_lint: string;
+	export const npm_package_devDependencies__typescript_eslint_eslint_plugin: string;
+	export const npm_config_registry: string;
+	export const npm_package_devDependencies_eslint_plugin_svelte3: string;
+	export const npm_config_node_gyp: string;
+	export const PATH: string;
+	export const npm_package_name: string;
+	export const NODE: string;
+	export const CHOKIDAR_USEPOLLING: string;
+	export const npm_package_devDependencies_eslint: string;
+	export const npm_lifecycle_script: string;
+	export const npm_package_scripts_test: string;
+	export const npm_package_devDependencies__sveltejs_kit: string;
+	export const npm_package_version: string;
+	export const npm_lifecycle_event: string;
+	export const NODE_PATH: string;
+	export const npm_package_scripts_build: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const npm_package_devDependencies_tslib: string;
+	export const npm_package_scripts_format: string;
+	export const PWD: string;
+	export const npm_execpath: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const npm_package_scripts_preview: string;
+	export const npm_package_devDependencies_prettier_plugin_svelte: string;
+	export const NODE_ENV: string;
+	export const INIT_CWD: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		USE_POLLING: string;
+		npm_config_user_agent: string;
+		NODE_VERSION: string;
+		HOSTNAME: string;
+		YARN_VERSION: string;
+		npm_package_devDependencies_vite: string;
+		npm_node_execpath: string;
+		SHLVL: string;
+		HOME: string;
+		npm_package_devDependencies__typescript_eslint_parser: string;
+		npm_package_devDependencies_eslint_config_prettier: string;
+		npm_package_devDependencies_svelte_preprocess: string;
+		npm_package_devDependencies_svelte_check: string;
+		npm_package_scripts_check: string;
+		npm_config_engine_strict: string;
+		npm_package_devDependencies_typescript: string;
+		npm_package_scripts_dev: string;
+		npm_package_devDependencies__playwright_test: string;
+		npm_package_devDependencies_prettier: string;
+		npm_package_type: string;
+		npm_package_private: string;
+		npm_package_scripts_check_watch: string;
+		npm_package_devDependencies__sveltejs_adapter_node: string;
+		npm_package_scripts_lint: string;
+		npm_package_devDependencies__typescript_eslint_eslint_plugin: string;
+		npm_config_registry: string;
+		npm_package_devDependencies_eslint_plugin_svelte3: string;
+		npm_config_node_gyp: string;
+		PATH: string;
+		npm_package_name: string;
+		NODE: string;
+		CHOKIDAR_USEPOLLING: string;
+		npm_package_devDependencies_eslint: string;
+		npm_lifecycle_script: string;
+		npm_package_scripts_test: string;
+		npm_package_devDependencies__sveltejs_kit: string;
+		npm_package_version: string;
+		npm_lifecycle_event: string;
+		NODE_PATH: string;
+		npm_package_scripts_build: string;
+		npm_package_devDependencies_svelte: string;
+		npm_package_devDependencies_tslib: string;
+		npm_package_scripts_format: string;
+		PWD: string;
+		npm_execpath: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		npm_package_scripts_preview: string;
+		npm_package_devDependencies_prettier_plugin_svelte: string;
+		NODE_ENV: string;
+		INIT_CWD: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: string]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
